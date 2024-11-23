970b5c03250d1ca21e9d4fc80e4a39504f41029d
Dr-T <xandertang@qq.com>
2024-11-23 17:30:03 +0800

    更新模型列表

--- app/constant.ts
+++ app/constant.ts
@@ -292,32 +292,22 @@ export const DEFAULT_TTS_VOICES = [
 ];
 
 const openaiModels = [
-  "gpt-3.5-turbo",
-  "gpt-3.5-turbo-1106",
-  "gpt-3.5-turbo-0125",
-  "gpt-4",
-  "gpt-4-0613",
-  "gpt-4-32k",
-  "gpt-4-32k-0613",
-  "gpt-4-turbo",
-  "gpt-4-turbo-preview",
   "gpt-4o",
   "gpt-4o-2024-05-13",
   "gpt-4o-2024-08-06",
   "gpt-4o-2024-11-20",
   "chatgpt-4o-latest",
   "gpt-4o-mini",
-  "gpt-4o-mini-2024-07-18",
-  "gpt-4-vision-preview",
-  "gpt-4-turbo-2024-04-09",
-  "gpt-4-1106-preview",
+  "gpt-4o-mini-2024-07-18"
   "dall-e-3",
   "o1-mini",
   "o1-preview",
+  "claude-3-5-sonnet-20240620@OpenAI",
+  "claude-3-5-sonnet-20241022@OpenAI",
+  "gemini-1.5-pro-latest@OpenAI",
 ];
 
 const googleModels = [
-  "gemini-1.0-pro",
   "gemini-1.5-pro-latest",
   "gemini-1.5-flash-latest",
   "gemini-exp-1114",
@@ -327,13 +317,8 @@ const googleModels = [
 ];
 
 const anthropicModels = [
-  "claude-instant-1.2",
-  "claude-2.0",
-  "claude-2.1",
-  "claude-3-sonnet-20240229",
   "claude-3-opus-20240229",
   "claude-3-opus-latest",
-  "claude-3-haiku-20240307",
   "claude-3-5-haiku-20241022",
   "claude-3-5-haiku-latest",
   "claude-3-5-sonnet-20240620",
